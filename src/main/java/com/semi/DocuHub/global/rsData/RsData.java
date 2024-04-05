2b1af30b5e808663678877f4b5371b7b42d3ac0e
hdYang <gmlehd0201@gmail.com>
2024-04-05 16:38:41 +0900

    baseCode

---
+++ src/main/java/com/semi/DocuHub/global/rsData/RsData.java
@@ -0,0 +1,30 @@
+package com.semi.DocuHub.global.rsData;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RsData <T> {
+
+    private String resultCode;
+    private String msg;
+    private T data;
+    private Boolean isSuccess;
+    private Boolean isFail;
+
+    public static <T> RsData<T> of( String resultCode, String msg, T data ){
+
+        Boolean isSuccess = resultCode.startsWith("S-");
+
+        return new RsData<>(resultCode,msg,data,isSuccess,!isSuccess);
+    }
+
+    public static <T> RsData<T> of( String resultCode, String msg){
+
+        Boolean isSuccess = resultCode.startsWith("S-");
+
+        return new RsData<>(resultCode,msg,null,isSuccess,!isSuccess);
+    }
+
+}
