683e904f934a1ba8286f7cab8bf31d65e03c7110
hdYang <gmlehd0201@gmail.com>
2024-04-05 15:05:00 +0900

    first commit

---
+++ src/main/java/com/semi/DocuHub/DocuHubApplication.java
@@ -0,0 +1,13 @@
+package com.semi.DocuHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semi/DocuHub/DocuHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semi.DocuHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
